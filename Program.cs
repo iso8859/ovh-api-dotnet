334dc120b00562f27b1f8117ae8e5db0f825c611
Remi THOMAS <remi@xtware.com>
2017-10-19 19:56:00 +0200

    API Demo

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using Ookii.CommandLine;
+using System;
+
+namespace ovh.api
+{
+    class Arguments
+    {
+        [CommandLineArgument(DefaultValue = "%ovh-ak%", IsRequired = false)]
+        public string AK { get; set; }
+        [CommandLineArgument(DefaultValue = "%ovh-as%", IsRequired = false)]
+        public string AS { get; set; }
+        [CommandLineArgument(DefaultValue = "%ovh-ck%", IsRequired = false)]
+        public string CK { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CommandLineParser parser = new CommandLineParser(typeof(Arguments));
+                Arguments a = (Arguments)parser.Parse(args);
+
+                a.AK = Environment.ExpandEnvironmentVariables(a.AK);
+                a.AS = Environment.ExpandEnvironmentVariables(a.AS);
+                a.CK = Environment.ExpandEnvironmentVariables(a.CK);
+
+                Rest r = new Rest(a.AK, a.AS, a.CK);
+                dynamic lineState = r.GetAsync<JObject>("https://eu.api.ovh.com/1.0/telephony/rt6-ovh-7/line/0033132465798/options").Result;
+                if ((bool)lineState.forwardNoReply)
+                    Console.WriteLine($"Redirect if no reply {lineState.forwardNoReplyNumber} {lineState.forwardNoReplyDelay}(s)");
+                else if ((bool)lineState.forwardUnconditional)
+                    Console.WriteLine($"Unconditional forward {lineState.forwardUnconditionalNumber}");
+
+                lineState.forwardUnconditionalNumber = "0033123456789";
+                lineState.forwardUnconditional = true;
+                lineState = r.PutAsync<JObject>("https://eu.api.ovh.com/1.0/telephony/rt6-ovh-7/line/0033123456789/options", lineState).Result;
+
+                // Second possible syntax
+                r.PutAsync<JObject>("https://eu.api.ovh.com/1.0/telephony/rt6-ovh-7/line/0033123456789/options",
+                    new { forwardUnconditional = true, forwardUnconditionalNumber = "0033123456789" }
+                    ).Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Rest.cs
@@ -0,0 +1,109 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ovh.api
+{
+    public class Rest
+    {
+        string _ak, _as, _ck;
+        public Rest(string AK, string AS, string CK)
+        {
+            _ak = AK;
+            _as = AS;
+            _ck = CK;
+        }
+
+        public static string[] HexTbl = Enumerable.Range(0, 256).Select(v => v.ToString("x2")).ToArray();
+        public static string ToHex(IEnumerable<byte> array)
+        {
+            StringBuilder s = new StringBuilder();
+            foreach (var v in array)
+                s.Append(HexTbl[v]);
+            return s.ToString();
+        }
+        public static string ToHex(byte[] array)
+        {
+            StringBuilder s = new StringBuilder(array.Length * 2);
+            foreach (var v in array)
+                s.Append(HexTbl[v]);
+            return s.ToString();
+        }
+
+        public static string HashSHA1(string sInputString)
+        {
+            return ToHex(System.Security.Cryptography.SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(sInputString)));
+        }
+
+        public static int TimeStamp()
+        {
+            return (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+
+        public string Signature(string method, string query, string body, int timestamp)
+        {
+            //calcul de la signature
+            return "$1$" + HashSHA1(_as + "+" + _ck + "+" + method + "+" + query + "+" + body + "+" + timestamp);
+        }
+
+        public HttpClient Client()
+        {
+            var result = new HttpClient();
+            foreach (var h in Headers)
+                result.DefaultRequestHeaders.Add(h.Key, h.Value);
+            return result;
+        }
+
+        public Dictionary<string, string> Headers = new Dictionary<string, string>();
+
+        public async Task<HttpResponseMessage> SendAsync(HttpMethod method, string query, string body = null)
+        {
+            var request = new HttpRequestMessage(method, query);
+            if (body != null)
+                request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+            var client = Client();
+            client.DefaultRequestHeaders.Add("X-Ovh-Application", _ak);
+            client.DefaultRequestHeaders.Add("X-Ovh-Consumer", _ck);
+            int timestamp = TimeStamp();
+            client.DefaultRequestHeaders.Add("X-Ovh-Signature", Signature(method.Method.ToUpper(), query, body, timestamp));
+            client.DefaultRequestHeaders.Add("X-Ovh-Timestamp", timestamp.ToString());
+
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+            if (!response.IsSuccessStatusCode && response.StatusCode != System.Net.HttpStatusCode.NotFound)
+            {
+                throw new Exception("Request failed:" + await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+            }
+            return response;
+        }
+
+        public async Task<T> GetAsync<T>(string uri)
+        {
+            var response = await SendAsync(HttpMethod.Get, uri);
+            response.EnsureSuccessStatusCode();
+            string s = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(s);
+        }
+
+        public async Task<T> PostAsync<T>(string uri, object data = null)
+        {
+            var response = await SendAsync(HttpMethod.Post, uri, JsonConvert.SerializeObject(data));
+            response.EnsureSuccessStatusCode();
+            var s = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(s);
+        }
+
+        public async Task<T> PutAsync<T>(string uri, object data = null)
+        {
+            var response = await SendAsync(HttpMethod.Put, uri, JsonConvert.SerializeObject(data));
+            response.EnsureSuccessStatusCode();
+            var s = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(s);
+        }
+    }
+}
