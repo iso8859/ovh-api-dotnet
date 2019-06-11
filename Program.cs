48fedd3781b171ed6bd922732b2ab5ae71bd06e1
Remi THOMAS <remi@xtware.com>
2019-06-11 09:37:56 +0200

    Better await management from synchronous main.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using Ookii.CommandLine;
 using System;
+using System.Threading.Tasks;
 
 namespace ovh.api
 {
@@ -15,6 +16,26 @@ namespace ovh.api
     }
     class Program
     {
+        static async Task VersionA(Rest r)
+        {
+            dynamic lineState = await r.GetAsync<JObject>("https://eu.api.ovh.com/1.0/telephony/rt6-ovh-7/line/0033132465798/options");
+            if ((bool)lineState.forwardNoReply)
+                Console.WriteLine($"Redirect if no reply {lineState.forwardNoReplyNumber} {lineState.forwardNoReplyDelay}(s)");
+            else if ((bool)lineState.forwardUnconditional)
+                Console.WriteLine($"Unconditional forward {lineState.forwardUnconditionalNumber}");
+
+            lineState.forwardUnconditionalNumber = "0033123456789";
+            lineState.forwardUnconditional = true;
+            lineState = await r.PutAsync<JObject>("https://eu.api.ovh.com/1.0/telephony/rt6-ovh-7/line/0033123456789/options", lineState);
+        }
+
+        static async Task VersionB(Rest r)
+        {
+            await r.PutAsync<JObject>("https://eu.api.ovh.com/1.0/telephony/rt6-ovh-7/line/0033123456789/options",
+                new { forwardUnconditional = true, forwardUnconditionalNumber = "0033123456789" }
+                );
+
+        }
         static void Main(string[] args)
         {
             try
@@ -27,20 +48,10 @@ namespace ovh.api
                 a.CK = Environment.ExpandEnvironmentVariables(a.CK);
 
                 Rest r = new Rest(a.AK, a.AS, a.CK);
-                dynamic lineState = r.GetAsync<JObject>("https://eu.api.ovh.com/1.0/telephony/rt6-ovh-7/line/0033132465798/options").Result;
-                if ((bool)lineState.forwardNoReply)
-                    Console.WriteLine($"Redirect if no reply {lineState.forwardNoReplyNumber} {lineState.forwardNoReplyDelay}(s)");
-                else if ((bool)lineState.forwardUnconditional)
-                    Console.WriteLine($"Unconditional forward {lineState.forwardUnconditionalNumber}");
 
-                lineState.forwardUnconditionalNumber = "0033123456789";
-                lineState.forwardUnconditional = true;
-                lineState = r.PutAsync<JObject>("https://eu.api.ovh.com/1.0/telephony/rt6-ovh-7/line/0033123456789/options", lineState).Result;
+                Task.Run(async () => await VersionA(r)).GetAwaiter().GetResult();
+                Task.Run(async () => await VersionB(r)).GetAwaiter().GetResult();
 
-                // Second possible syntax
-                r.PutAsync<JObject>("https://eu.api.ovh.com/1.0/telephony/rt6-ovh-7/line/0033123456789/options",
-                    new { forwardUnconditional = true, forwardUnconditionalNumber = "0033123456789" }
-                    ).Wait();
             }
             catch (Exception ex)
             {
